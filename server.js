3896524dcea285a42412e2f854c3ce729e215e87
kehdcode <you@example.com>
2020-11-21 15:51:38 +0100

    commit

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(process.env.PORT || 5000)
+const shortUrl = require('./Models/shortUrl')
+
+mongoose.connect('mongodb://localhost/urlshortner', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}) 
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+app.post('/shortUrls', async (req, res) =>{
+    await shortUrl.create({ full: req.body.fullUrl})
+    res.redirect('/')
+
+})
+app.listen(process.env.PORT || 1500) 
